dc76c35c0c701af4c80727f19f823b0931b9b318
Jack161223 <167942356+Jack161223@users.noreply.github.com>
2024-04-23 19:35:49 -0600

    Add files via upload

---
+++ ExamSpiderman.js
@@ -0,0 +1,11 @@
+var elige = prompt("Te gusta Miles Morales?");
+switch(elige){
+    case "Si":
+        alert("Aqui tienes informacion sobre él");
+    break; 
+
+    case "No":
+    alert("Que triste, aqui tienes informacionn sobre esl");
+    break;
+
+}
